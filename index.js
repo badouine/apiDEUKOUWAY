fd880ef69d0bc617d9b6f882cf0505f85cac10ec
badouine <nzbadouine@gmail.com>
2022-06-12 02:36:15 +0000

    Connected to mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to MONGODB");
+    } catch (error) {
+        throw error ;
+    }
+};
 
 app.listen(8800, () => {
+    connect();
     console.log("Connected to backend server");
 })
