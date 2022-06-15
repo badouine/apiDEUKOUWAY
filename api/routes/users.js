114b59fe3954558756079ddf1b1b49425fdaf4ff
badouine <nzbadouine@gmail.com>
2022-06-15 03:47:15 +0000

    check user

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,12 +5,16 @@ import {
   getUser,
   getUsers,
 } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/checkauthentication", verifyToken, (req, res, next) => {
-    res.send("hello user, you are logged in!");
+  res.send("hello user, you are logged in!");
+});
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+  res.send("you are logged in! always");
 });
 
 // UPDATE
