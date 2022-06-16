21f8190efea2c021c8934fece7d5f6dcc5f98d8b
badouine <nzbadouine@gmail.com>
2022-06-16 02:37:37 +0000

    room routes

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,6 +1,26 @@
 import express from 'express';
+import { createRoom, deleteRoom, getRooms, updateRoom } from '../controllers/room';
 
 const router = express.Router();
 
+
+
+// CREATE
+router.post("/", verifyAdmin, createRoom);
+
+// UPDATE
+router.put("/:id",verifyAdmin, updateRoom);
+
+// DELETE
+
+router.delete("/:id",verifyAdmin, deleteRoom);
+
+// GET
+
+router.get("/:id", getRooms);
+
+// GET ALL
+
+router.get("/", getRooms);
 
 export default router
