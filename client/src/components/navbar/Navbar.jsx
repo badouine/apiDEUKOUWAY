990e7e7e7edd4e3c80c6ea5814704dbe5ed4269f
badouine <n>
2022-06-21 22:22:33 +0000

    item featured

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,20 @@
 import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-        <div className="navContainer">
-            <span className="logo">DEUKOUWAY</span>
-            <div className="navItems">
-                <button className="navButton">Inscription</button>
-                <button className="navButton">Se connecter</button>
-            </div>
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit" , textDecoration:"none"}}>
+          <span className="logo">DEUKOUWAY</span>
+        </Link>
+        <div className="navItems">
+          <button className="navButton">Inscription</button>
+          <button className="navButton">Se connecter</button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
