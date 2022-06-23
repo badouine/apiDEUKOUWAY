18669c4ed85603978101f0b753cc28b65e5ebbd0
badouine <n>
2022-06-23 14:15:45 +0000

    reservation close set

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -1,7 +1,19 @@
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./reserve.css";
 
 const Reserve = ({ setOpen, hotelId }) => {
-  return <div className="reserve">Reserve</div>;
+  return (
+    <div className="reserve">
+      <div className="rContainer">
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          className="rClose"
+          onClick={() => setOpen(false)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Reserve;
