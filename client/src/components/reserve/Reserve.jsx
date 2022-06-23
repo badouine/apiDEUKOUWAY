4d3037559d604288c3a49b02f6e3d6318960178e
badouine <n>
2022-06-23 22:55:20 +0000

    available room

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -1,12 +1,34 @@
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { SearchContext } from "../../context/SearchContext";
 import useFetch from "../../hooks/useFetch";
 import "./reserve.css";
 
 const Reserve = ({ setOpen, hotelId }) => {
   const [selectedRooms, setSelectedRooms] = useState([]);
   const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
+  const { dates } = useContext(SearchContext);
+  const getDatesInRange = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const date = new Date(start.getTime());
+
+    let list = [];
+
+    while (data <= end) {
+      list.push(new Date(date).getTime());
+      date.setDate(date.getDate() + 1);
+    }
+    return list;
+  };
+  const alldates = getDatesInRange(dates[0].startDate, dates[0].endDate);
+  const isAvailable = (roomNumber) => {
+    const isFound = roomNumber.unavailableDates.some((date) =>
+      alldates.includes(new Date(date).getTime())
+    );
+    return !isFound
+  };
   const handleSelect = (e) => {
     const checked = e.target.checked;
     const value = e.target.value;
@@ -17,7 +39,7 @@ const Reserve = ({ setOpen, hotelId }) => {
     );
   };
 
-  console.log(selectedRooms);
+  const handleClick = () => {};
   return (
     <div className="reserve">
       <div className="rContainer">
@@ -44,6 +66,7 @@ const Reserve = ({ setOpen, hotelId }) => {
                   type="checkbox"
                   value={roomNumber._id}
                   onChange={handleSelect}
+                  disabled={!isAvailable(roomNumber)}
                 />
               </div>
             ))}
