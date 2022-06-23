d3764c5c9fcf52dc6cfd52d51a93ac0a2d318b0c
badouine <n>
2022-06-23 16:51:23 +0000

    romm data

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -1,8 +1,10 @@
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useFetch from "../../hooks/useFetch";
 import "./reserve.css";
 
 const Reserve = ({ setOpen, hotelId }) => {
+  const { data, loading, error } = useFetch(`hotels/room/${hotelId}`);
   return (
     <div className="reserve">
       <div className="rContainer">
@@ -12,6 +14,17 @@ const Reserve = ({ setOpen, hotelId }) => {
           onClick={() => setOpen(false)}
         />
         <span>Selectionner chambre:</span>
+        {data.map((item) => (
+          <div className="rItem">
+            <div className="rItemInfo">
+              <div className="rTitle">{item.title}</div>
+              <div className="rDesc">{item.desc}</div>
+              <div className="rMax">
+                Pop maximum:<b>{item.maxPeople}</b>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
